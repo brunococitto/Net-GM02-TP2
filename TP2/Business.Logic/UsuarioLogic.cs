acb0654869017eda82c8518c34aef83e8e00e5e1
Bruno Cocitto <bcocitto@gmail.com>
2021-04-29 12:41:15 -0300

    TP2 - Lab 3 (Unidad 2) - Etapa 3 y comenzada la 4
    Faltan implementar los métodos del menú

---
+++ TP2/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic : BusinessLogic
+    {
+        public UsuarioAdapter UsuarioData { get; set; }
+        public UsuarioLogic()
+        {
+            UsuarioData =  new UsuarioAdapter();
+        }
+        public List<Usuario> GetAll()
+        {
+            return UsuarioData.GetAll();
+        }
+        public Usuario GetOne(int id)
+        {
+            return UsuarioData.GetOne(id);
+        }
+        public void Delete(int id)
+        {
+            UsuarioData.Delete(id);
+        }
+        public void Save(Usuario usuario)
+        {
+            UsuarioData.Save(usuario);
+        }
+    }
+}
---
+++ TP2/UI.Console/Usuario.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Consola
+{
+    public class Usuario
+    {
+        public UsuarioLogic UsuarioNegocio { get; set; }
+        public Usuario()
+        {
+            UsuarioNegocio = new UsuarioLogic();
+        }
+        public void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Menú de opciones");
+            Console.WriteLine("1 - Listado General");
+            Console.WriteLine("2 - Consulta");
+            Console.WriteLine("3 - Agregar");
+            Console.WriteLine("4 - Modificar");
+            Console.WriteLine("5 - Eliminar");
+            Console.WriteLine("6 - Salir");
+            switch (LeerOpcionMenu())
+            {
+                case 1:
+                    ListadoGeneral();
+                    break;
+                case 2:
+                    Consultar();
+                    break;
+                case 3:
+                    Agregar();
+                    break;
+                case 4:
+                    Modificar();
+                    break;
+                case 5:
+                    Eliminar();
+                    break;
+            }
+        }
+        private int LeerOpcionMenu()
+        {
+            int opcion = 6;
+            try
+            {
+                Console.Write("Ingrese la opción deseada: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                if (opcion < 1 | opcion > 6)
+                {
+                    throw new Exception("La opción ingresada es incorrecta!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                LeerOpcionMenu();
+            }
+            return opcion;
+        }
+        private void ListadoGeneral() { }
+        private void Consultar() { }
+        private void Agregar() { }
+        private void Modificar() { }
+        private void Eliminar() { }
+        private void MostrarDatos() { }
+    }
+}
