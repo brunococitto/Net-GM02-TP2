a7efc444d6ab6198f817b667b87da385061b05e5
Santiago <smasetto@hotmail.com>
2021-05-27 13:11:13 -0300

    TP2 - Lab 4 - Etapa 1 (Listado de usuarios)

---
+++ TP2/UI.Desktop/Usuarios.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        private void tlUsuarios_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        public void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.dgvUsuarios.AutoGenerateColumns = false;
+            this.dgvUsuarios.DataSource = ul.GetAll();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
